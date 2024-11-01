4464df08daa6dd466f90686585ae7b885cdb6666
Evanjalin-K <jojaev510@gmail.com>
2024-11-01 15:43:15 +0530

    utilities added --cors enabled --cookie parsed  --body parsed --request logged --error endpoint handled

--- app.js
+++ app.js
@@ -2,9 +2,30 @@
 
 const express = require('express');
 const userRouter = require('./routes/userRoutes');
+const requestLogger = require('./utils/logger');
+const unknownEndpoint = require('./utils/Error');
+const cors = require('cors');
+const cookieParser = require('cookie-parser')
+//const morgan = require('morgan');
 const app = express();
+
+
+// Morgan default library for log data
+//app.use(morgan('dev'))
+//middleware
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials:true
+}));
+app.use(cookieParser());
+
+app.use(express.json());
+
+app.use(requestLogger);
 
 // use is used to add the middleware, it will be used according to the order
 app.use('/api/v1', userRouter);
+
+app.use(unknownEndpoint);
 
 module.exports = app;
